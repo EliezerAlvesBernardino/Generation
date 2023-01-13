102e2da69326e5120fe16ee3f30962bde2216033
eabernardino <eliezer.abernardino@mercadolivre.com>
2023-01-13 14:42:23 -0300

    Aula laços de Repetições

--- JAVA_/src/LacosRepeticoes/Aula.java
+++ JAVA_/src/LacosRepeticoes/Aula.java
@@ -1,29 +1,97 @@
 package LacosRepeticoes;
 
+import java.util.Scanner;
+
 public class Aula {
 
     public static void main(String[] args){
+
         /*
-        System.out.println("HELLO WORLD!");
+        //Laços de Repetição FOR
+
+        String nome;
         int contador;
 
-        //Para(int = contador = 0; contador<3; contador ++ >
-        for (contador = 0; contador < 3; contador++){
-            System.out.println("Bom dia!");
+        Scanner leia = new Scanner(System.in);
+
+        for (contador = 1; contador < 4; contador++){
+            System.out.println("\nDigite o " + contador + " nome: ");
+            nome = leia.nextLine();
+            System.out.println("o " + contador + " nome é: " + nome);
         }
-        System.out.println("Lá na fazendinha!");
-        System.out.println(contador);
 
          */
 
-        //enquanto
-        int contador = 0;
+        /*
+        //Laço de Repetição FOR - Tabuada
 
-        while (contador < 3){
-            System.out.println("Hoje vai dar bom!");
-            contador ++;
+        int numero, contador;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite a Tabuada que você deseja calcular: ");
+        numero = leia.nextInt();
+
+        for (contador = 1; contador <= 10; contador ++){
+            System.out.println(numero + " x " + contador + " = " + numero * contador);
         }
 
+         */
+/*
+        // Laço de repetição WHILE
+        String continua = "s";
+        int numero1, numero2, resultado;
+        Scanner leia = new Scanner(System.in);
+
+        while (!continua.equals("n")){
+            System.out.println("Digite o primeiro valor: ");
+            numero1 = leia.nextInt();
+            System.out.println("Digite o seugundo valor: ");
+            numero2 = leia.nextInt();
+
+            resultado = numero1 + numero2;
+
+            System.out.println("O resultado da soma é: " + resultado);
+
+            System.out.println("\n++++++++++++++++ MENU *******************");
+            System.out.println("\nDeseja somar 2 valores ? ");
+            System.out.println("\nDigite s para sim OU digite n para não: ");
+            continua = leia.next();
+            System.out.println("============================================");
+        }
+
+
+
+
+        int resultado, numero, contador = 1;
+        Scanner leia = new Scanner(System.in);
+
+        while(contador < 4 ){
+            System.out.println("Digite o " + contador + "numero: ");
+            numero = leia.nextInt();
+
+            resultado = numero * 3;
+            System.out.println(numero + " x 3 = " + resultado);
+            System.out.println("\n============================================");
+            contador ++;
+        }
+        System.out.println("\t\t________ Fim do Programa ____________");
+
+ */
+        //Laços de repetições DO WHILE
+
+        int numero, resultado, contador = 3;
+        Scanner leia = new Scanner(System.in);
+
+        do {
+            System.out.println("\nDigite um número inteiro: ");
+            numero = leia.nextInt();
+
+            resultado = numero * 5;
+
+            System.out.println("\nO resultado da multiplicação é: " + resultado);
+
+        } while (contador <= 2);
+        
 
     }
 }
