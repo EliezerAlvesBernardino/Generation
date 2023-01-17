d90a890555d51cc0c41a3c1fccd5c1aeea40dd29
eabernardino <eliezer.abernardino@mercadolivre.com>
2023-01-16 23:02:34 -0300

    up

---
+++ Java--/Classes_Objetos/src/AtividadesEntrega/Entrega.java
@@ -0,0 +1,132 @@
+package AtividadesEntrega;
+
+import Atividade_01.Cliente;
+import Atividade_02.Funcionario;
+import Atividade_03.Produto;
+
+public class Entrega {
+    public static void main(String[] args){
+
+        /*
+                                Atividade 01
+
+        package Atividade_01;
+
+        public class Cliente {
+            public String nome;
+            public int idade;
+            public int rg;
+            public String banco;
+            public int conta;
+
+
+
+
+            public Cliente(String nome, int idade, int rg, String banco, int conta){
+                this.nome = nome;
+                this.idade = idade;
+                this.rg = rg;
+                this.banco = banco;
+                this.conta = conta;
+            }
+
+            void visualizar(){
+                System.out.println("Nome do cliente:  " + nome);
+                System.out.println("Idade do cliente: " + idade);
+                System.out.println("RG do cliente:    " + rg);
+                System.out.println("Banco do cliente: " + banco);
+                System.out.println("Conta do cliente: " + conta);
+            }
+
+        }
+
+        Atividade_01.Cliente cliente01 = new Atividade_01.Cliente("Joel", 22, 123321, "itaú", 2213);
+
+        cliente01.visualizar();
+
+         */
+
+
+
+        /*
+                            Atividade 02
+
+
+
+
+        package Atividade_02;
+
+        public class Funcionario {
+            public String  nome;
+            public int     idade;
+            public int     registro;
+            public String  funcao;
+            public boolean status;
+
+            public Funcionario(String nome, int idade, int registro, String funcao, boolean status){
+                this.nome     = nome;
+                this.idade    = idade;
+                this.registro = registro;
+                this.funcao   = funcao;
+                this.status   = status;
+
+            }
+
+            void visualizar(){
+                System.out.println("Nome do funcionário:            " + nome);
+                // System.out.println("Idade do funcionário:           " + idade);
+                // System.out.println("Registro do fuincionário:       " + registro);
+                System.out.println("Cargo que o funcionário exerce: " + funcao);
+                // System.out.println("Funcionário está ativo ?        " + status);
+            }
+
+        }
+        Atividade_02.Funcionario funcionario_01 = new Atividade_02.Funcionario("Joel", 44,123, "Encarregado",true);
+
+        funcionario_01.nome = "José";
+        funcionario_01.funcao = "Analista";
+        funcionario_01.visualizar();
+
+         */
+
+        /*
+                                Atividade 03
+
+
+        package Atividade_03;
+
+        public class Produto {
+            public String nome;
+            public String categoria;
+            public int    codigo;
+            public double preco;
+            public int    garantia;
+
+            public Produto(String nome, String categoria, int codigo, double preco, int garantia){
+                this.nome      = nome;
+                this.categoria = categoria;
+                this.codigo    = codigo;
+                this.preco     = preco;
+                this.garantia  = garantia;
+            }
+
+            void visualizar(){
+                System.out.println("Produto:           " + nome);
+                System.out.println("Categoria:         " + categoria);
+                System.out.println("Código do produto: " + codigo);
+                System.out.println("Preço:             " + preco);
+                System.out.println("Garantia:          " + garantia);
+
+            }
+
+        Atividade_03.Produto jogos = new Atividade_03.Produto("Fifa", "Esportes", 123, 97.00, 1);
+
+            jogos.nome = "Mario Bross";
+            jogos.preco = 80.90;
+
+        jogos.visualizar();
+
+         */
+    }
+
+}
