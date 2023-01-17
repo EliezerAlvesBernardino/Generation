9f1b1ed81653a25b4fb7aca445e421f73a1ef5c4
eabernardino <eliezer.abernardino@mercadolivre.com>
2023-01-16 22:47:28 -0300

    Modificacao na pasta para entrega

---
+++ Java--/Classes_Objetos/src/AtividadesEntrega/Atividade_03/Atividade_03/TestaGame.java
@@ -0,0 +1,12 @@
+package AtividadesEntrega.Atividade_03.Atividade_03;
+
+public class TestaGame {
+    public static void main(String[] args){
+        Produto jogos = new Produto("Fifa", "Esportes", 123, 97.00, 1);
+
+        jogos.nome = "Mario Bross";
+        jogos.preco = 80.90;
+
+        jogos.visualizar();
+    }
+}
